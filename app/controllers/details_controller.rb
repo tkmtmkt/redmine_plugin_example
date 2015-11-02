506731475ca04532b932d3722d3361c1cdf57d7f
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-11-02 13:46:48 +0900

    権限判定追加

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,6 +1,8 @@
 class DetailsController < ApplicationController
   unloadable
-
+  before_filter :authorize_global, except: [:show, :edit, :update]
+  before_filter :find_project_by_project_id, :authorize, only: [:show, :edit, :update]
+  menu_item :detail, only: [:show, :edit, :update]
 
   def index
   end
