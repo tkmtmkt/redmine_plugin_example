8b3aab5cea537ad4db9c9a05a92dd1337e8b1fb1
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-11-02 12:15:10 +0900

    権限＆メニュー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  resources :details, only: [:index]
+  resources :projects do
+    resource :detail, only: [:show, :edit, :update]
+  end
+end
--- init.rb
+++ init.rb
@@ -5,4 +5,17 @@ Redmine::Plugin.register :redmine_plugin_example do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+
+  permission :manage_details, { details: [:index] }, require: :member
+
+  project_module :redmine_plugin_example do
+    permission :manage_detail, { details: [:show, :edit, :update] }, require: :member
+  end
+
+  menu :application_menu, :details, { controller: :details, action: :index },
+    caption: :label_details, before: :issues, param: :project_id
+
+  menu :project_menu, :detail, { controller: :details, action: :show },
+    caption: :label_details, before: :issues, param: :project_id
 end
