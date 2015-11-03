4dec4ade268c8c3a296f618ffc3bf9eafd4e43fb
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-11-03 18:11:47 +0900

    プロジェクトへの関連付け

--- db/migrate/001_create_details.rb
+++ db/migrate/001_create_details.rb
@@ -1,6 +1,7 @@
 class CreateDetails < ActiveRecord::Migration
   def change
     create_table :details do |t|
+      t.references :project, index: true, foreign_key: { name: 'fk_projects_on_project_id' }
       t.string :year
     end
   end
